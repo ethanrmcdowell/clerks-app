753104a8802c0d08ae66e27ae944b77b3f230e75
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2024-01-16 16:09:09 -0500

    garage add reactive form

--- src/app/garage-add/garage-add.component.ts
+++ src/app/garage-add/garage-add.component.ts
@@ -1,21 +1,37 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-garage-add',
   standalone: true,
-  imports: [MatCardModule, MatInputModule, MatDatepickerModule, FormsModule, MatButtonModule],
+  imports: [MatCardModule, MatInputModule, MatDatepickerModule, ReactiveFormsModule, FormsModule, MatButtonModule],
   templateUrl: './garage-add.component.html',
   styleUrl: './garage-add.component.css'
 })
 export class GarageAddComponent {
-
+  addForm = new FormGroup({
+    startDate: new FormControl(''),
+    endDate: new FormControl(''),
+    formatStartDate: new FormControl(''),
+    formatEndDate: new FormControl(''),
+    permitNum: new FormControl(''),
+    name: new FormControl(''),
+    address: new FormControl(''),
+    zip: new FormControl(''),
+    telephone: new FormControl(''),
+    section: new FormControl(''),
+    orgName: new FormControl(''),
+    orgAddress: new FormControl(''),
+    orgPhone: new FormControl(''),
+    notes: new FormControl('')
+  })
 
   addSale() {
-
+    console.log(this.addForm.value);
   }
 }
