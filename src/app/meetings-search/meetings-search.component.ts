5dfa12c2cf095f7af8004a9952abc5cdd079d8ae
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2024-01-17 11:14:34 -0500

    meeting search page styling and form

--- src/app/meetings-search/meetings-search.component.ts
+++ src/app/meetings-search/meetings-search.component.ts
@@ -1,12 +1,40 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-meetings-search',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, FormsModule, MatRadioModule, MatButtonModule, ReactiveFormsModule, MatDatepickerModule, MatInputModule, MatNativeDateModule, MatFormFieldModule],
   templateUrl: './meetings-search.component.html',
   styleUrl: './meetings-search.component.css'
 })
 export class MeetingsSearchComponent {
+  constructor(private datePipe: DatePipe) {}
 
+  searchForm = new FormGroup({
+    startDate: new FormControl(''),
+    endDate: new FormControl(''),
+    formatStartDate: new FormControl(''),
+    formatEndDate: new FormControl(''),
+    keyword1: new FormControl(''),
+    keyword2: new FormControl(''),
+    idNumber: new FormControl('')
+  });
+
+  searchData() {
+    const start = this.datePipe.transform(this.searchForm.get('startDate')?.value, "yyyy-MM-dd", "GMT");
+    const end = this.datePipe.transform(this.searchForm.get('endDate')?.value, "yyyy-MM-dd", "GMT");
+    this.searchForm.get('formatStartDate')?.setValue(start === null ? '' : start);
+    this.searchForm.get('formatEndDate')?.setValue(end === null ? '' : end);
+
+    console.log(this.searchForm.value);
+  }
 }
